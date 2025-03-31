8f58e40843e1ed14b677741f9a12b4e8110656e7
Arun.k <arun.k@Arunks-MacBook-Air.local>
2025-03-31 13:24:38 +0530

    data-base connected and added data sucessfully

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const routes = require('./routes');
-
+const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user')
 
-const port = 2000;
+app.post('/signup',async (req,res)=>{
+    const user = new User({
+        firstName : "Arun",
+        lastName : "k",
+        emailId : "arun.k@gmail.com",
+        password : "****",
+        age : 18,
+        gender : "male"
 
-app.use('/home',(req,res)=>{
-    res.send("Hello from home");
-});
+    });
 
-// use the routes
-app.use('/',routes);
+    try{
+        await user.save();
+        res.send("user created sucessfully");
+    }
+    catch(err){
+        res.status(400).send("something Went Wrong" + err.message)
+    }
+        
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
 })
+
+connectDB().then(()=>{
+    console.log("Connected to database");
+    app.listen(2000,()=>{
+        console.log(`Server is running on port ${2000}`);
+    })
+}).catch((err)=>{
+    console.log("Error connecting to database",err);
+}) ;
+
